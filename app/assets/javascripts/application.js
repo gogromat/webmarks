833d28a68d685fd4a31cbfca2f3e0c12c36d7a2f
Alexander Astafurov <gogromat@fastmail.fm>
2012-09-26 17:21:27 -0400

    Sign up users initial set-up

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function(){
+
+    $('#hero-unit-close').click(function(){
+        $('#main-hero-unit').hide();
+    });
+
+});
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,47 +6,51 @@ describe "Static Pages" do
 
 	# shared example group (RSpec 2)
 	shared_examples_for "all static pages" do
-		it { should have_selector('title', 	text: page_title) }
-		it { should have_selector('h1', 	text: heading   ) }	
+		it { should have_selector('title', 	text: full_title(page_title) ) }
+		it { should have_selector('h1', 	  text: heading   ) }
 	end
-
 
 	describe "Home Page" do
 		before { visit '/' }
-
-		let(:page_title){ 'Ruby'}
-		let(:heading)   { 'Welcome'}
-
+		let(:page_title){ ''}
+		let(:heading)   { 'Welcome' }
 		it_should_behave_like "all static pages"
 		it { should_not have_selector('title', text: "| Home")}
 	end
 
-	describe "About Page" do
-		before { visit about_path }
-
-		let(:page_title){ 'About'}
+  describe "About Page" do
+    before { visit about_path }
+		let(:page_title){ 'More About Webmarks'}
 		let(:heading)   { 'About'}
-
 		it_should_behave_like "all static pages"
 	end
 
 	describe "Contact Page" do
 		before { visit contact_path }
-
 		let(:page_title){ 'Contact'}
 		let(:heading)   { 'Contact'}
-
 		it_should_behave_like "all static pages"
 	end
 
 	describe "Help Page" do
 		before { visit help_path }
-
 		let(:page_title){ 'Help'}
 		let(:heading)   { 'Help'}
-
 		it_should_behave_like "all static pages"
 	end
 
+  it "should have right links on the layout" do
+    visit root_path
+    page.should have_selector 'title', text: full_title('')
+    click_link "About"
+    page.should have_selector 'title', text: full_title('More About Webmarks')
+    visit root_path
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    visit root_path
+    click_link "Home"
+    click_link "No, thanks!"
+    page.should have_selector 'title', text: full_title('')
+  end
 
 end
