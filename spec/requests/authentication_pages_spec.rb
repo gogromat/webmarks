91afe45cd9c897392118e4a9f70174ae6ca2fe79
Alexander Astafurov <gogromat@fastmail.fm>
2012-09-29 22:40:06 -0400

    For heroku

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,129 @@
+require 'spec_helper'
+
+describe "Authentication Pages" do
+
+  subject { page }
+
+  describe "signin page" do
+
+    before { visit signin_path }
+
+    it { should have_selector('title', text: "Sign in")}
+    it { should have_selector('h1',    text: "Sign in")}
+    it { should have_link('Sign up',   href:  signup_path)}
+  end
+
+  describe "signin" do
+
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', tex: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in(user) }
+
+      it { should     have_selector('title',  text: 'Webmark')            }
+
+      it { should     have_link('Users',      href: users_path)           }
+      it { should     have_link('Profile',    href:  user_path(user))     }
+      it { should     have_link('Settings',   href:  edit_user_path(user))}
+      it { should     have_link('Sign out',   href:  signout_path)        }
+
+      it { should_not have_link('Sign in',    href:  signin_path)         }
+
+
+      describe "followed by signout" do
+        before { click_link "Sign out"   }
+        it { should have_link("Sign in") }
+      end
+    end
+  end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          sign_in user
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            page.should have_selector('title', text: 'Update')
+          end
+        end
+      end
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          # update action
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in')}
+        end
+
+      end
+
+
+    end
+
+    describe "as a wrong user" do
+      let(:user)       { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "another@here.com") }
+
+      before { sign_in user }
+
+      describe "visiting the Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should_not have_selector('title', text: 'Update') }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
+      end
+
+    end
+
+    describe "as a non-admin user" do
+      let(:user)      { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before  { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+
+    end
+
+  end
+
+
+end
