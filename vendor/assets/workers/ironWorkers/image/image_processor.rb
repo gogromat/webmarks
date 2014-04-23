387493b0dd648a38d2bdc7f2d78ac4d3fbbd9d88
Alexander Astafurov <gogromat@gmail.com>
2014-04-22 21:33:23 -0400

    removed some info

--- vendor/assets/workers/ironWorkers/image/image_processor.rb
+++ vendor/assets/workers/ironWorkers/image/image_processor.rb
@@ -7,8 +7,8 @@ client = IronWorkerNG::Client.new()
 client.tasks.create(
     'ImageProcessor',
     image_url:  "http://dev.iron.io/images/iron_pony.png",
-    aws_access: "AKIAIRPO5UVCSY3543IA",
-    aws_secret: "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",
+    aws_access: "",
+    aws_secret: "",
     aws_s3_bucket_name: "webmarks",
 )
 
@@ -17,8 +17,8 @@ def upload_file(filename = "output.png")
   puts "\nUploading the file to s3..."
   s3 = Aws::S3Interface.new(@params['aws_access'], @params['aws_secret'])
 
-  #aws_access         = "AKIAIRPO5UVCSY3543IA"
-  #aws_secret         = "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"
+  #aws_access         = ""
+  #aws_secret         = ""
   #aws_s3_bucket_name = "webmarks"
   #s3 = Aws::S3Interface.new(aws_access, aws_secret)
   #s3.create_bucket(aws_s3_bucket_name)
